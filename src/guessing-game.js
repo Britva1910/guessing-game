7d25e9606cc9f9380438e7a332b90090404cc7da
Britva1910 <bbritva1910@gmail.com>
2021-08-19 16:34:26 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,24 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.hulf = Math.ceil((this.min + this.max) / 2);
+        return this.hulf;
     }
 
     lower() {
-
+        this.max = this.hulf;
     }
 
     greater() {
-
+        this.min = this.hulf;
     }
 }
 
